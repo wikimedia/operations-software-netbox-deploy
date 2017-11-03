490f303ebeba35c8cc54560db48c2a8ba0b5e761
Arzhel Younsi <ayounsi@wikimedia.org>
2017-11-03 12:57:04 -0700

    Update checks scripts to reflect netbox location
    Change-Id: I9c6baf5499e04cd29daa1afad568b83a92c25f58

--- scap/checks/netbox_setup.sh
+++ scap/checks/netbox_setup.sh
@@ -5,11 +5,12 @@ set -o nounset
 set -o pipefail
 
 VENV=/srv/deployment/netbox/venv
-NETBOX_ROOT=/srv/deployment/netbox/netbox
+NETBOX_ROOT=/srv/deployment/netbox/deploy/netbox
 PYTHON=${VENV}/bin/python3
 
 # Run database migration
 python3 manage.py migrate
+${PYTHON} ${NETBOX_ROOT}/netbox/manage.py migrate
 
 # Collect static files
 ${PYTHON} ${NETBOX_ROOT}/netbox/manage.py collectstatic --no-input
