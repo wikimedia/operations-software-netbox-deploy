f94ce0f5b2fdafe461f9fc485b300059072adb9c
John Bond <github@johnbond.org>
2021-06-08 16:00:50 +0200

    cas: add cas_configuration symlink
    Bug: T244849
    Change-Id: Ia943f1cb68fdd84bdad8106f056e1475511b06c6

--- scap/checks/linkconf.sh
+++ scap/checks/linkconf.sh
@@ -10,6 +10,11 @@ if [ ! -f ${DEPLOY_DIR}/src/netbox/netbox/configuration.py ]; then
 	ln -s /etc/netbox/configuration.py ${DEPLOY_DIR}/src/netbox/netbox/configuration.py
 fi
 
-if [ ! -f ${DEPLOY_DIR}/src/netbox/netbox/ldap_config.py ]; then
+
+if [ ! -f ${DEPLOY_DIR}/src/netbox/netbox/ldap_config.py ] && [ -f /etc/netbox/ldap.py ]; then
     ln -s /etc/netbox/ldap.py ${DEPLOY_DIR}/src/netbox/netbox/ldap_config.py
+fi
+
+if [ ! -f ${DEPLOY_DIR}/src/netbox/netbox/cas_configuration.py ] && [ -f /etc/netbox/cas_configuration.py ]; then
+    ln -s /etc/netbox/cas_configuration.py ${DEPLOY_DIR}/src/netbox/netbox/cas_configuration.py
 fi
