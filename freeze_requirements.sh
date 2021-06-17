c7762f8d447174b6115757cf60fbd7b39225a8d8
Riccardo Coccioli <rcoccioli@wikimedia.org>
2021-06-17 12:15:32 +0200

    Add pip's dependencies to the generated wheels
    * When generating the artifacts include pip's dependencies too so that
      there will be no surprises when deploying the artifacts. This includes
      pip itself, wheel, distribute and setuptools.
    * Rebuilt artifacts with the newly included packages.
    
    Change-Id: Id6e73b452a887ff872598ad4a6e17b3a599bcc68

--- freeze_requirements.sh
+++ freeze_requirements.sh
@@ -12,7 +12,7 @@ REQUIREMENTS=${BASE}/requirements.txt
 REQUIREMENTS_FIXED=${BASE}/frozen-requirements.txt
 
 pip3 install -r "$REQUIREMENTS"
-pip3 freeze --local --requirement "$REQUIREMENTS_FIXED" > "$REQUIREMENTS_FIXED"
+pip3 freeze --all --local --requirement "$REQUIREMENTS_FIXED" > "$REQUIREMENTS_FIXED"
 # https://github.com/pypa/pip/issues/4668
 sed -i '/pkg-resources==0.0.0/d' "$REQUIREMENTS_FIXED"
 echo "${REQUIREMENTS_FIXED} updated, please commit it to git."
