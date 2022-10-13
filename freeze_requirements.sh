a7c306e973b7a09c2afd8a67570b14075e08a99a
Arzhel Younsi <ayounsi@wikimedia.org>
2022-10-13 12:59:40 +0200

    Re-generate wheels with pip list
    To remove the @ file after ntc-netbox-plugin-metrics-ext
    
    Bug: T311052
    Change-Id: Ie91a43ef564cb04ccc208138ef9517fd67f8cc5f

--- freeze_requirements.sh
+++ freeze_requirements.sh
@@ -13,7 +13,7 @@ REQUIREMENTS_FIXED=${BASE}/frozen-requirements-${DISTRO}.txt
 
 pip3 install -r "$REQUIREMENTS"
 pip3 install ${BASE}/plugins/ntc-netbox-plugin-metrics-ext
-pip3 freeze --all --local --requirement "$REQUIREMENTS_FIXED" > "$REQUIREMENTS_FIXED"
+pip3 list --format=freeze --local > "$REQUIREMENTS_FIXED"
 # https://github.com/pypa/pip/issues/4668
 sed -i '/pkg-resources==0.0.0/d' "$REQUIREMENTS_FIXED"
 echo "${REQUIREMENTS_FIXED} updated, please commit it to git."
